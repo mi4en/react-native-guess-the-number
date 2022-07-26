61780cb26550ce12188feebaf5a9346c38dd6ecd
Mario <marionikolov87@gmail.com>
2022-07-26 22:09:13 +0300

    initial commit; create placeholder screens;

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, View, TextInput, Text } from 'react-native'
+
+const StartGameScreen = () => {
+	return (
+		<View>
+			<TextInput />
+			<Text>SS</Text>
+		</View>
+	)
+}
+
+export default StartGameScreen
+
+const styles = StyleSheet.create({})
